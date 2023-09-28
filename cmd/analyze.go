fd9d3b3031d4eb7b918a142a74f8d29a4d5d9bc6
Guru Coyote <gc@testbild.tv>
2023-09-28 14:53:22 +0200

    Refactor analyze command to remove unnecessary imports and validate input in root command.

--- cmd/analyze.go
+++ cmd/analyze.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 	"github.com/spf13/cobra"
 )
 
@@ -11,19 +12,14 @@ var topCmd = &cobra.Command{
 	Long: `This command will go through the global log entries slice, and count how many entries there are for each unique value of the field specified.
 It will then output a sorted top n list from the findings, with count and the field-name as columns.`,
 	Args: cobra.ExactArgs(1),
-import (
-	"fmt"
-	"strings"
-	"github.com/spf13/cobra"
-)
 
+
+Run: func(cmd *cobra.Command, args []string) {
 var validFields = map[string]string{
 	"IP": "IP", "TIMESTAMP": "Timestamp", "STATUSCODE": "StatusCode", "BYTESSENT": "BytesSent",
 	"REQUESTMETHOD": "RequestMethod", "REQUESTURL": "RequestURL", "REQUESTPROTOCOL": "RequestProtocol",
 	"REFERRER": "Referrer", "USERAGENT": "UserAgent", "CHECKSUM": "Checksum",
 }
-
-Run: func(cmd *cobra.Command, args []string) {
 	upperArg := strings.ToUpper(args[0])
 	if _, ok := validFields[upperArg]; !ok {
 		keys := make([]string, 0, len(validFields))
