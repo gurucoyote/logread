7f868e68899f8a71e8fc60fd15b1dfb322fae6e2
Guru Coyote <gc@testbild.tv>
2023-09-28 12:15:23 +0200

    aider: Removed JSON output from root command's run function.
    # Aider chat conversation:
    
    ASSISTANT: To achieve this, we will create a new sub-command in the `cmd/json.go` file. This command will be responsible for outputting the content of the `LogEntries` global slice as a JSON formatted string. We will also modify the `cmd/root.go` file to remove the JSON output from the root command's run function.
    
    Here are the steps:
    
    1. Create a new file `cmd/json.go` and define a new command `jsonCmd` in it. This command will output the `LogEntries` global slice as a JSON formatted string.
    
    2. Modify the `cmd/root.go` file to remove the JSON output from the root command's run function.
    
    Let's start with the first step:
    
    ```go
    cmd/json.go
    <<<<<<< HEAD
    =======
    package cmd
    
    import (
    	"encoding/json"
    	"fmt"
    	"log"
    
    	"github.com/spf13/cobra"
    )
    
    var jsonCmd = &cobra.Command{
    	Use:   "json",
    	Short: "Output log entries as JSON",
    	Long:  `This command outputs the log entries as a JSON formatted string.`,
    	Run: func(cmd *cobra.Command, args []string) {
    		jsonData, err := json.MarshalIndent(LogEntries, "", "    ")
    		if err != nil {
    			log.Fatal(err)
    		}
    		fmt.Println(string(jsonData))
    	},
    }
    
    func init() {
    	rootCmd.AddCommand(jsonCmd)
    }
    >>>>>>> updated
    ```
    
    Now, let's modify the `cmd/root.go` file:
    
    ```go
    cmd/root.go
    <<<<<<< HEAD
    		fmt.Printf("Count of entries: %d\n", len(LogEntries))
    		jsonData, err := json.MarshalIndent(LogEntries, "", "    ")
    		if err != nil {
    			log.Fatal(err)
    		}
    		fmt.Println(string(jsonData))
    =======
    		fmt.Printf("Count of entries: %d\n", len(LogEntries))
    >>>>>>> updated
    ```
    
    With these changes, the JSON output will be handled by the new `json` command, and the root command will only print the count of log entries.

---
+++ cmd/json.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var jsonCmd = &cobra.Command{
+	Use:   "json",
+	Short: "Output log entries as JSON",
+	Long:  `This command outputs the log entries as a JSON formatted string.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		jsonData, err := json.MarshalIndent(LogEntries, "", "    ")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(jsonData))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(jsonCmd)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -78,11 +78,6 @@ It provides useful insights and analytics from your log files.`,
 			}
 		}
 		fmt.Printf("Count of entries: %d\n", len(LogEntries))
-		jsonData, err := json.MarshalIndent(LogEntries, "", "    ")
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(string(jsonData))
 	},
 }
 
